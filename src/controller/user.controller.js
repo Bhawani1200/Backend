60ce3d3795abcd9809444ce733d253e56066d560
Bhawani1200 <bhawanilimbu1200@gmail.com>
2024-02-05 17:08:13 +0545

    changed

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -28,17 +28,7 @@ try {
     console.log(error);
 }
 }
-const createProducts = async (req,res)=>{
-    console.log(req.body);
-  const newProduct =new Product(req.body)
-  try {
-    const Product =await newProduct.save();
-    res.status(200).send({Product})
-  } catch (error) {
-    res.status(400).send({Product,error})
-   console.log(error)
-  }
-}
+
 
 
 export {getUser,createUser,deleteUser}
