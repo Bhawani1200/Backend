a3c652fe9d6d12d78bf4091521d96025630f20a2
Bhawani1200 <bhawanilimbu1200@gmail.com>
2024-02-14 10:10:02 +0545

    new files

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,22 +1,39 @@
+import  { generateHashPassword,jwtLogin } from "../middleware/jwt.js";
 import { User } from "../models/User.js";
-
+import jwt from 'jsonwebtoken'
 const getUser=(req,res)=>{
     res.send({Message:"Getting users request"});
 }
 const createUser = async(req,res)=>{
     console.log(req.body);
     const { name,email,password } =req.body;
+    const hashPassword=await generateHashPassword(password);
     const user=new User({
         name,
         email,
-        password
+        password:hashPassword
     })
     try {
+    
         const users=await user.save();
-        res.send({data :users})
+        const token= await jwtLogin(users);
+        console.log(token);
+        res.send({data :users,token:token})
     } catch (error) {
         console.log(error)
     }
+}
+const loginUser =async (req,res)=>{
+    const user= await User.find({email:req.body.email});
+    if(!user)
+    {
+        res.status(400).send({message:"Invalid user"});
+    }
+    const token = req.headers.authorization.split(" ")[0];
+    console.log(token);
+    var decoded=await jwt.verify(token,process.env.SECRET);
+    console.log(decoded);
+    res.send({decoded:decoded})
 }
 const deleteUser = async (req,res)=>{
 const id=req.params.id;
@@ -29,4 +46,4 @@ try {
 }
 }
 
-export {getUser,createUser,deleteUser}
+export {getUser,createUser,deleteUser,loginUser}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,11 @@
 import express  from "express";
-import  {getUser ,createUser,deleteUser  } from "../controller/user.controller.js";
+import  {getUser ,createUser,deleteUser, loginUser  } from "../controller/user.controller.js";
 // import {createProducts,deleteProduct} from "../controller/product.controller.js";
 
 const userRouter=express.Router();
 userRouter.get("/",getUser);
 userRouter.post("/",createUser);
+userRouter.post("/login",loginUser);
 // userRouter.post("/product",createProducts);
 // userRouter.delete("/:id",deleteProduct);
 userRouter.delete("/:id",deleteUser);
